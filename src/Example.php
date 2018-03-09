126b528d45901682371675fa5fa4672f0df83188
Gitau Karugi <gkarugi@gmail.com>
2018-03-09 17:53:38 +0300

    Initial commit. Package set up

---
+++ src/Example.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Imarishwa\MpesaBridge;
+
+class Example
+{
+
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Imarishwa\MpesaBridge\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+
+}
