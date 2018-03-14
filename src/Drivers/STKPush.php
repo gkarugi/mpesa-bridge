7eae00a44c781aa36fddd04eb131ef8e435488f5
Gitau Karugi <gkarugi@gmail.com>
2018-03-15 00:57:58 +0300

    added stk push functionality. what is remaining is validation and handling the callback

--- src/Drivers/STKPush.php
+++ src/Drivers/STKPush.php
@@ -2,6 +2,10 @@
 
 namespace Imarishwa\MpesaBridge\Drivers;
 
+use Carbon\Carbon;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+
 class STKPush extends AbstractDriver
 {
     /**
@@ -12,17 +16,47 @@ class STKPush extends AbstractDriver
     protected $message;
 
     /**
-     * Specify the SMS message.
-     *
-     * @pram string $message
-     *
-     * @return $this
+     * @throws \Imarishwa\MpesaBridge\Exceptions\MissingBaseApiDomainException
      */
-    public function message($message)
+    public function message()
     {
-        dd($this->accessToken);
-        $this->message = $message.' Here';
+        dd($this->buildRequest());
+    }
 
-        return $this;
+    /**
+     * @throws \Imarishwa\MpesaBridge\Exceptions\MissingBaseApiDomainException
+     * @throws \App\Exceptions\InvalidCredentialsException
+     */
+    public function buildRequest()
+    {
+        $time = Carbon::now()->format('YmdHis');
+        $shortCode = $this->config['short_code'];
+        $passkey   = $this->config['passkey'];
+
+        $password  = \base64_encode($shortCode . $passkey . $time);
+
+        $client = new Client([
+            'headers' => [
+                'Authorization' => 'Bearer '.$this->authenticate(),
+                'Accept'        => 'application/json',
+            ],
+            'json' => [
+                'BusinessShortCode' => $this->config['short_code'],
+                'Password' => $password,
+                'Timestamp' => $time,
+                'TransactionType' => 'CustomerPayBillOnline',
+                'Amount' => 20,
+                'PartyA' => '254727357218',
+                'PartyB' => $shortCode,
+                'PhoneNumber' => '254727357218',
+                'CallBackURL' => 'https://revenue.localtunnel.me/callback',
+                'AccountReference' => 'Imarishwa County',
+                'TransactionDesc' => 'payment for county'
+            ]
+        ]);
+
+        $response = $client->send(new Request($this->config['callback_method'], $this->getApiBaseUrl().MPESA_STK_PUSH_URL));
+        $body = \json_decode($response->getBody());
+        dd($body);
     }
 }
